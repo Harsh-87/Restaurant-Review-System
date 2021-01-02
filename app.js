6aa73e4f3dc49bbf30eca937edb425b04942a0bd
Harsh Kothari <harsh.77424@gmail.com>
2021-01-02 10:47:58 +0530

    Minor issues fixxed : 2 Jan,21

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
-app.use('/imageUpload',uploadRouter);
+app.use('/imageUpload', uploadRouter);
 app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
@@ -61,8 +61,9 @@ app.use(function (err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(res.statusCode || 500);
+  // res.render('error');   //Use this when you have a error page in your views directory with error.jade file to show the error
+  res.send(err.message);
 });
 
 module.exports = app;
